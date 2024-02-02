c2128b7874f8520ae0067e9ca256b48c6de906cb
CodeVenger1 <154542222+CodeVenger1@users.noreply.github.com>
2024-02-02 16:23:01 +0530

    Update 23CE01052_Assignment3_3.c

--- 23CE01052_Assignment3_3.c
+++ 23CE01052_Assignment3_3.c
@@ -1,16 +1,13 @@
 #include<stdio.h>
 int main()
 {
-    float x1,x2,x3,y1,y2,y3;
-    printf("\n Enter 1st coordinate");
-    scanf("%f %f",&x1,&y1);
-    printf("\n Enter 2nd coordinate");
-    scanf("%f %f",&x2,&y2);
-    printf("\n Enter 3rd coordinate");
-    scanf("%f %f",&x3,&y3);
-    if((y1-y2)/(x1-x2)==(y2-y3)/(x2-x3))
-    printf("\n The three points are on a straight line")
+    float x1,x2,x3;
+    printf("\n Enter three lengths:");
+    scanf("%f %f %f",&x1,&x2,&x3);
+    if(x1+x2>x3&&x1+x3>x2&&x2+x3>x1)
+        printf("\n Three sides represent a triangle");
     else
-    printf("\n The three points are not on a straight line");
+        printf("\n They doesnt form triangle");
     return 0;
+
 }
